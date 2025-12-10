d56d5a29d87de407acb8c935b2451c1d29c36735
selerqc <agustincharles34@gmail.com>
2025-12-10 18:02:34 +0800

    update CORS origin to use the correct production URL

--- backend/src/config/startup/cors.js
+++ backend/src/config/startup/cors.js
@@ -2,7 +2,7 @@ import cors from 'cors'
 
 export function initCORS(app) {
   app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://sgod-medisync.vercel.app'],
+    origin: ['http://localhost:5173', 'http://localhost:4173', 'https://sgod-shs.vercel.app'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   }));
